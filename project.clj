b26735f02435ae06636724d44a3c2d9a2e2fcea2
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:44:45 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject org.vladimir/db-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/db-lib "0.1.0"
   :description "Database library"
-  :url "http://gitlab:1610/VladimirMarkovic86/db-lib"
+  :url "http://github.com/VladimirMarkovic86/db-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"])
+
